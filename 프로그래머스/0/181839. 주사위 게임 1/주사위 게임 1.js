034aa368ebd9666818fea164b57d34ce3a5468ef
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-04-04 23:52:48 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+   return (
+        (a % 2 === 1 && b % 2 === 1) ? a * a + b * b :
+        (a % 2 === 1 || b % 2 === 1) ? 2 * (a + b) :
+        Math.abs(a - b)
+    );
+}
