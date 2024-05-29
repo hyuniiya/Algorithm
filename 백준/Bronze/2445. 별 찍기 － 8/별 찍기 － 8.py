dea136dda046d636447bb33ddaa74dd8092ee94c
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-05-29 16:49:17 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -1,13 +1,7 @@
-N = int(input())
+n = int(input())
 
-# 첫 번째 줄부터 N번째 줄까지 출력합니다.
-for i in range(1, N + 1):
-    stars = '*' * i
-    spaces = ' ' * (2 * (N - i))
-    print(stars + spaces + stars)
+for i in range(1, n+1):  # 1부터 n까지 증가
+    print("*"*i+" "*(n-i)+" "*(n-i)+"*"*i)
 
-# N+1번째 줄부터 2N-1번째 줄까지 출력합니다.
-for i in range(N - 1, 0, -1):
-    stars = '*' * i
-    spaces = ' ' * (2 * (N - i))
-    print(stars + spaces + stars)
+for j in range(n-1, 0, -1):  # n-1부터 1까지 감소
+    print("*"*j+" "*(n-j)+" "*(n-j)+"*"*j)
