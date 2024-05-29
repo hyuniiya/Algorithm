7301cc2b08d0b61eb084051683b4b170d913794e
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-05-29 14:43:28 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,14 +1,13 @@
-from collections import Counter
+a, b, c = map(int, input().split())
 
-num_list = list(map(int, input().split())) # [1,1,2]
-count = Counter(num_list)  # {2:1, 1:2}
-most_common = count.most_common(1) # 빈도가 가장 높은 요소를 담은 리스트를 반환
 
-if most_common[0][1] == 3:
-    prize = 10000 + most_common[0][0] * 1000
-elif most_common[0][1] == 2:
-    prize = 1000 + most_common[0][0] * 100
+if a == b == c:
+    prize = 10000 + a * 1000
+elif a == b or a == c:
+    prize = 1000 + a * 100
+elif b == c:
+    prize = 1000 + b * 100
 else:
-    prize = max(num_list) * 100
+    prize = max(a, b, c) * 100
 
 print(prize)
