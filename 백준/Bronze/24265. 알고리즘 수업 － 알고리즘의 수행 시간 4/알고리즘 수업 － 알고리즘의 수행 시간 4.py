bb98531232537f89292377979de89c39270ae893
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-05-31 11:27:33 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
@@ -1,10 +1,9 @@
 def degree():
     n = int(input())
     sum = 0
-    for i in range(1, n):  # 1부터 n-1까지 반복
-        sum += (n - i)  # 각 i에 대해 (n - i)번 수행
+    for i in range(1, n):  
+        sum += (n - i)  
     
-    print(sum)
-    print(2)
+    print(sum, 2)
 
 degree()
