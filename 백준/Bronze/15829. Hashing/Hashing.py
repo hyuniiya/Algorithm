bffab048dee6eff260e720e1247f39a420436fe2
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-06-06 22:04:30 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,21 +1,7 @@
-# 문자열 수열 -> 정수로 변환
-import sys,string
-
-# 문자열의 길이
-input = sys.stdin.readline
 L = int(input())
-# 문자열
-string_input = input()
-r = 31
-M = 1234567891
-
-# 최종 해시 값 저장
+string = input()
 answer = 0
 
-# 각 문자의 값을 계산
 for i in range(L):
-    # 현재 문자의 값 위치 변환
-    value = string.ascii_lowercase.index(string_input[i]) + 1
-    answer += value * (r ** i)
-
-print(answer % M)
+    answer += (ord(string[i])-96) * (31 ** i) #아스키 코드 값을 돌려주는 ord함수
+print(answer % 1234567891)
