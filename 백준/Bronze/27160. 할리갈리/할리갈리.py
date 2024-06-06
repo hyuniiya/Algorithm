30ec8041d205b67e1bf25a2db59abe2a045ef210
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-06-06 12:43:25 +0900

    [Bronze II] Title: 할리갈리, Time: 116 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.py
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -1,21 +1,19 @@
-def solution():
-    N = int(input())
-    count = {}
+import sys
+from collections import defaultdict
 
-    # 과일 종류, 개수 분리
-    for i in range(N):
-        S,X = input().split()
-        fruit_count = int(X)
-        # 과일 종류가 이미 존재할 경우, 과일 개수 추가
-        if S in count:
-            count[S] += fruit_count
-        # 없을 경우, 과일 종류 추가
-        else:
-            count[S] = fruit_count
-    # 5개인지 확인
-    if 5 in count.values():
+def ring_bell():
+    input = sys.stdin.readline
+    n = int(input())
+
+    fruit_count = defaultdict(int) # {}
+
+    for i in range(n):
+        fruit, count = input().split() # ["BANANA" , "2", "PLUM", "4", "BANANA", "3"]
+        fruit_count[fruit] += int(count) # {"BANANA": 5, "PLUM": 4}
+
+
+    if  5 in fruit_count.values():
         print("YES")
     else:
         print("NO")
-
-solution()
+ring_bell()
