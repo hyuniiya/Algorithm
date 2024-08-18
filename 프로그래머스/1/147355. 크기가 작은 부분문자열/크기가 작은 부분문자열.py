2edabe02aff327cb973742749e15c8aa2b174ab0
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-08-18 23:17:30 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 6.55 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,11 +1,12 @@
 def solution(t, p):
-    m = len(p)  # p의 길이
-    p_num = int(p)  # p를 정수로 변환
-    count = 0
+    answer = 0
+    temp = ''
     
-    for i in range(len(t) - m + 1):
-        substring = t[i:i + m]  # 길이가 m인 부분 문자열 추출
-        if int(substring) <= p_num:  # 부분 문자열을 정수로 변환하여 비교
-            count += 1  # 조건을 만족하면 카운트 증가
-    
-    return count
+    for i in t:
+        temp += i 
+        
+        if len(temp) == len(p):
+            if int(temp) <= int(p):
+                answer+= 1
+            temp = temp[1:]
+    return answer
