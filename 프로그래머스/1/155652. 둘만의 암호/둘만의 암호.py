7eec658eb6536e7d4a0281abb7a700f8946c1675
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-08-19 21:00:27 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,8 @@
+def solution(s, skip, index):
+    answer = ""
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    for i in list(skip):
+        alpha = alpha.replace(i,"")
+    for a in s:
+        answer += alpha[(alpha.find(a) + index) % len(alpha)]
+    return answer
