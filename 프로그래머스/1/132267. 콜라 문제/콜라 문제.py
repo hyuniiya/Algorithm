5bb40a191543d973fa26f0ee151da91884105ae8
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-08-29 23:59:11 +0900

    [level 1] Title: 콜라 문제, Time: 0.74 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,10 @@
+def solution(a, b, n):
+    total_cola = 0
+    
+    while n >= a:
+        new_cola = (n // a) * b
+        total_cola += new_cola
+        
+        n = (n % a) + new_cola
+    
+    return total_cola
