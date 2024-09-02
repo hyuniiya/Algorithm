e64529cd85c11dacd458e7349ba4d07c35c6a90c
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-02 11:29:59 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,5 +1,5 @@
 def solution(a, b):
-    days = ['THU', 'FRI', 'SAT','SUN', 'MON', 'TUE','WED']
-    months = [31,29,31,30,31,30, 31, 31, 30, 31, 30, 31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    return days[(sum(months[:a-1])+b)%7]
+    return days[(sum(months[:a-1]) + (b - 1)) % 7]
