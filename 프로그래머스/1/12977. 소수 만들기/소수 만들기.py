3b330718df55c62fc60611a7e85ca9e58ebf8d81
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-06 15:58:33 +0900

    [level 1] Title: 소수 만들기, Time: 12.96 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,19 +1,23 @@
 import itertools
+import math
 
+# 소수 판별 함수 (제곱근까지만 확인)
 def prime(n):
     if n < 2:
         return 0
-    else:
-        for i in range(2,n):
-            if n % i == 0: 
-                return 0
-        return 1
-    
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return 0
+    return 1
+
 def solution(nums):
     answer = 0
-    combi = list(itertools.combinations(nums,3))
+    combi = itertools.combinations(nums, 3)
+    prime_cache = {}  
 
-    for i in range(len(combi)):
-        answer += prime(sum(combi[i]))
+    for comb in combi:
+        if sum(comb) not in prime_cache:
+            prime_cache[sum(comb)] = prime(sum(comb))
+        answer += prime_cache[sum(comb)]
     
     return answer
