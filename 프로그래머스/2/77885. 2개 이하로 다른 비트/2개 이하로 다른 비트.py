e83636d16ab7a0c86df6f07696f0852944dc47b4
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-06 12:11:43 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 341.92 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,11 +1,13 @@
 def solution(numbers):
-    def next_number(x):
-        if x & 1 == 0:  # 짝수인 경우
-            return x + 1
+    answer = []
+    
+    for number in numbers:
+        bin_num = list('0' + bin(number)[2:])
+        idx = ''.join(bin_num).rfind('0')
+        bin_num[idx] = '1'
         
-        # 연속된 1의 패턴 찾기
-        c = x & -x
-        r = x + c
-        return (((r ^ x) >> 2) // c) | r
-
-    return [next_number(num) for num in numbers]
+        if number % 2 == 1:
+            bin_num[idx + 1] = '0'
+            
+        answer.append(int(''.join(bin_num), 2))
+    return answer
