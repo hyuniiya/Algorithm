e760ac5c979d22e39a7372b6f56d45bcc18602f4
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-09 12:11:06 +0900

    [level 1] Title: 덧칠하기, Time: 20.04 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,19 +1,14 @@
 def solution(n, m, section):
-    intervals = []
-    for s in section:
-        if not intervals or s > intervals[-1][1]:
-            intervals.append([s, s])
-        else:
-            intervals[-1][1] = s
-    
     count = 0
     painted_until = 0
-    for start, end in intervals:
-        if start > painted_until:
-            count += 1
-            painted_until = min(start + m - 1, n)
-        while end > painted_until:
-            count += 1
-            painted_until = min(painted_until + m, n)
+    index = 0
+    
+    while index < len(section):
+        start = section[index]
+        end = min(start + m - 1, n)
+        count += 1
+        
+        while index < len(section) and section[index] <= end:
+            index += 1
     
     return count
