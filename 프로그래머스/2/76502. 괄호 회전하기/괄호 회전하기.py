7b64fcebe590caab134547ca045e8eeb88a9eb81
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-11 12:21:41 +0900

    [level 2] Title: 괄호 회전하기, Time: 60.44 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,18 +1,23 @@
-def is_valid(s):
+def is_valid_bracket_string(s):
     stack = []
-    brackets = {')': '(', ']': '[', '}': '{'}
+    bracket_pairs = {')': '(', ']': '[', '}': '{'}
+    
     for char in s:
         if char in '([{':
             stack.append(char)
         elif char in ')]}':
-            if not stack or stack.pop() != brackets[char]:
+            if not stack or stack.pop() != bracket_pairs[char]:
                 return False
+    
     return len(stack) == 0
 
 def solution(s):
     count = 0
-    for i in range(len(s)):
+    n = len(s)
+    
+    for i in range(n):
         rotated = s[i:] + s[:i]
-        if is_valid(rotated):
+        if is_valid_bracket_string(rotated):
             count += 1
+    
     return count
