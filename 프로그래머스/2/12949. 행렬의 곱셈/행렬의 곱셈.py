a2f3908f3f45eb426392022e7c1bf86ccecfb32d
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-20 10:49:52 +0900

    [level 2] Title: 행렬의 곱셈, Time: 48.91 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,8 +1,13 @@
 def solution(arr1, arr2):
-    return [
-        [
-            sum(a * b for a, b in zip(row, col))
-            for col in zip(*arr2)
-        ]
-        for row in arr1
-    ]
+    result = [] 
+
+    for row in arr1:
+        new_row = []
+        for col in zip(*arr2):
+            sum_value = 0 
+            for a, b in zip(row, col):
+                sum_value += a * b
+            new_row.append(sum_value)
+        result.append(new_row)
+
+    return result
