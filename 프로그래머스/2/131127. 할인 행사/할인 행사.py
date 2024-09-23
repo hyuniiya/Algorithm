e4c105337d4bcf2073e5ff7191c2faaa4a25563e
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-09-23 11:52:46 +0900

    [level 2] Title: 할인 행사, Time: 62.86 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,12 +1,10 @@
-from collections import Counter
-
 def solution(want, number, discount):
-    target = Counter(dict(zip(want, number)))
-    days = 0
-    
-    for i in range(len(discount) - 9):
-        current = Counter(discount[i:i+10])
-        if current == target:
-            days += 1
-    
+    def check(b):
+        for idx, w in enumerate(want):
+            if b.count(w) < number[idx]: return False
+        return True
+
+    basket, days = discount.copy(), 0
+    for idx, d in enumerate(discount):
+        if check(basket[idx:idx+10]): days+=1
     return days
