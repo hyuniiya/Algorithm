4aac460a160327d75b1dc9c62d1b7a96fdea84e0
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-11-03 20:28:08 +0900

    [level 2] Title: 땅따먹기, Time: 358.10 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -2,9 +2,12 @@ def solution(land):
     for row in range(1, len(land)):
         prev_row = land[row-1]
         curr_row = land[row]
-
+        
         for col in range(4):
-            other_cols = [prev_row[i] for i in range(4) if i != col]
+            other_cols = [] 
+            for i in range(4): 
+                if i != col:
+                    other_cols.append(prev_row[i])
             curr_row[col] += max(other_cols)
 
     return max(land[-1])
