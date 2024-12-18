e0c7120e060b5a9ff66c7aa012ae6633fd5e770b
Juhyun Park <124558854+hyuniiya@users.noreply.github.com>
2024-12-18 19:38:52 +0900

    [level 0] Title: 배열 만들기 6, Time: 85.16 ms, Memory: 18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    stk = []
+    for num in arr:
+        if stk and stk[-1] == num:
+            stk.pop()
+        else:
+            stk.append(num)
+    return stk if stk else [-1]
